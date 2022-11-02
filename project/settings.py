3a027346cb71b1079c999ac9def62e498ec893a0
integerJI <jjs9536@gmail.com>
2022-11-02 15:57:57 +0900

    Feat: static file update

--- project/settings.py
+++ project/settings.py
@@ -119,6 +119,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / 'static/'
+
+STATICFILES_DIRS = []
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
